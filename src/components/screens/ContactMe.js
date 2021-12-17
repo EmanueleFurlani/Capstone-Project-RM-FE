a94f542575910a92af9151d1fa8cf41075a45fcd
emanuele <emanuelefurlani.go@gmail.com>
2021-12-17 03:05:54 +0100

    add info admin

--- src/components/screens/ContactMe.js
+++ src/components/screens/ContactMe.js
@@ -4,10 +4,10 @@ const ContactMe = () => {
     return (
     
             <div className="container-1" style={{marginTop:"190px"}}>
-
                 <img id="contactimg" src="https://res.cloudinary.com/dqffc0h5e/image/upload/v1639700829/monti%20fvg/1607799123445_fj4clc.jpg"  width="180" height="180" alt="contactlogo"/>
                 <h3 style={{fontFamily:"Libre Baskerville"}}>Emanuele Furlani</h3>
                         <div className="divIno"> 
+                        <p>Developer and admin app contact:</p>
                             <ul className="rul">
                                 <li><a href="https://www.instagram.com/lelego85/">Instagram</a></li>
                                 <li><a href="https://mail.google.com/mail/?view=cm&fs=1&to=emanuelefurlani.go@gmail.com">Gmail</a></li>
